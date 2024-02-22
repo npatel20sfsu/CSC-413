f387203142f69148f65ed94b48ef72b6313f1a13
Nidhey Patel <124939775+npatel20sfsu@users.noreply.github.com>
2024-02-21 16:35:36 -0800

    Delete Assignment-1(A) directory

--- Assignment-1(A)/src/ListWithIteratorInterface.java
+++
@@ -1,9 +0,0 @@
-// Interface extending ListInterface and adding Iterator functionality
-package arraylistwithiterator;
-
-import java.util.Iterator;
-
-public interface ListWithIteratorInterface<T> extends ListInterface<T>, Iterable<T> {
-    Iterator<T> getIterator();
-}
-
